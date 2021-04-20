f4634ded788adb070a5d15548d6dfa3fda69dfde
Ulan Rakymzhanov <ulan@carta.healthcare>
2021-04-19 20:46:59 -0500

    some more code

---
+++ 01_js-intro/index.js
@@ -0,0 +1,29 @@
+// ACTIVITY console.log -----------------------
+console.log(4 + 3);
+// alert(4 + 3);
+// document.write(4 + 3);
+
+// STATEMENTS -----------------------
+/*
+  console.log('Hi There'); // statement 1
+  console.log('Cat is beatuful'); // statement 2
+  console.log(12); // statement 3
+*/
+// research about documenation using multiline comment
+
+// VARIABLES -----------------------
+
+var language = 'My favorite language is JavaScript';
+console.log(language);
+// reassigning a value
+language = 'My favortie language is Java';
+console.log(language);
+
+var sum = 4 + 3;
+console.log(sum);
+
+// declaration and initialization
+var box; // declare
+box = 2021; // initialize
+
+console.log(box);
