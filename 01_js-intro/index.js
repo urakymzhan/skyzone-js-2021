48b271183ccb85f8c442eaa56da7b1818bc3f012
Ulan Rakymzhanov <ulan@carta.healthcare>
2021-04-19 22:01:48 -0500

    some more code

--- 01_js-intro/index.js
+++ 01_js-intro/index.js
@@ -25,5 +25,67 @@ console.log(sum);
 // declaration and initialization
 var box; // declare
 box = 2021; // initialize
-
 console.log(box);
+
+// ACTIVITY variables -----------------------
+var age = 20;
+var year = 2000;
+console.log(age, year);
+// alert(age, year);
+// document.write(age, year);
+
+// DATA TYPES -----------------------
+// To check a type of any vafriable use: typeof
+
+// a. Number
+var a = 10;
+var b = 3;
+var summy = a + b; // addition
+var subby = a - b; // substraction
+var mully = a * b; // multiplication
+var divvy = a / b; // division
+var moddy = a % b; // modulus
+
+console.log('addition: ', summy);
+console.log('substraction: ', subby);
+console.log('multiplication: ', mully);
+console.log('division: ', divvy);
+console.log('modulus', moddy);
+
+// var inc = a++; // post increment
+// var dec = b--; // post decrement
+// console.log(`inc`, inc);
+// console.log(`dec`, dec);
+console.log(`a`, a++);
+console.log(`b`, b--);
+
+// b. String
+// loosly typed language
+
+// var message = 'Dont\'t throw a trash';
+// var message = "Dont't throw a trash";
+
+var name = 'Ulan';
+var lastName = 'Doe';
+var fullName = name + ' ' + lastName;
+console.log(fullName);
+
+// Concatate Number and String
+var num = 20;
+var num2 = 10;
+var text = 'Text';
+
+console.log(num + num2 + text); // 20(num) + 10(num) = 30 + Text(string) = 30Text
+console.log(text + num + num2); // Text(string) + 20 = Text20 + 10 = Text2010
+
+// c. Boolean
+// it only holds 2 values: true or false
+var isClassStarted = true;
+// var isClassStarted = false;
+
+// Assigment
+// Research and read about:
+// - null
+// - undefined
+// - Symbol
+// We will discuss before the next class
