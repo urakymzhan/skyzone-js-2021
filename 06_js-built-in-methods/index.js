09e76650cfe662b8e67659f197e5215593452027
Ulan Rakymzhanov <ulan@carta.healthcare>
2021-04-29 23:42:27 -0500

    bultin

--- 06_js-built-in-methods/index.js
+++ 06_js-built-in-methods/index.js
@@ -176,3 +176,180 @@ console.log(banana2);
 const banana3 = str1.substr(7, 6);
 console.log(banana3);
 console.log(str1.substr(7, 10));
+
+// Activity
+let sentence4 = 'Hey this is skyzone js basics';
+const idxOfSkyzone = sentence4.indexOf('skyzone');
+sentence4 = sentence4.replace('skyzone', 'SkyZone');
+sentence4 += ' almost the end'; // concat()
+sentence4 = sentence4.slice(0, sentence4.indexOf(' end'));
+const words = sentence4.split(' ');
+// console.log(words);
+
+/**
+ * Array methods
+ */
+
+// isArray()
+const cities = ['NY', 'LA'];
+console.log(Array.isArray(cities));
+console.log(typeof cities);
+
+// length
+console.log(cities.length);
+
+// toString()
+console.log(cities.toString());
+
+// join()
+console.log(cities.join());
+console.log(cities.join('*'));
+console.log(cities.join('/'));
+
+// pop()
+const fruits = ['Banana', 'Orange', 'Apple', 'Mango'];
+// fruits.pop(); // remove the last element
+const mango = fruits.pop(); // remove and store
+console.log(`fruits`, fruits);
+console.log(`mango`, mango);
+
+// push()
+const fruits2 = ['Banana', 'Orange', 'Apple', 'Mango'];
+fruits2.push('Kiwi'); // adds kiwi to the end
+console.log(fruits2);
+const y = fruits2.push('Kiwi');
+console.log(y); // length of new array
+console.log(fruits2);
+
+// shift()
+const fruits3 = ['Banana', 'Orange', 'Apple', 'Mango'];
+fruits3.shift();
+console.log(fruits3);
+
+// unshift
+const fruits4 = ['Banana', 'Orange', 'Apple', 'Mango'];
+fruits4.unshift('Lemon');
+console.log(fruits4);
+
+// splice()
+// array.splice(at index, how many to be removed, element to be inserted)
+const months = ['Jan', 'March', 'April', 'June'];
+months.splice(1, 0, 'Feb');
+// inserts at index 1
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "June"]
+months.splice(4, 1, 'May');
+// replaces 1 element at index 4
+console.log(months);
+// expected output: Array ["Jan", "Feb", "March", "April", "May"]
+
+// concat()
+const arr1 = ['Cecilie', 'Lone'];
+const arr2 = ['Emil', 'Tobias', 'Linus'];
+const arr3 = ['Robin', 'Morgan'];
+const myChildren = arr1.concat(arr2, arr3); // Concatenates arr1 with arr2 and arr3
+// const myChildrenES6 = [...arr1, ...arr2, ...arr3];
+
+// slice()
+var fruits9 = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango', 'Orange'];
+var citrus = fruits9.slice(1);
+var citrus2 = fruits9.slice(1, 3);
+
+// indexOf && lastIndexOf - similar to string methods
+console.log(fruits9.indexOf('Orange'));
+console.log(fruits9.lastIndexOf('Orange'));
+
+// Iteration Methods
+
+// forEach()
+fruits9.forEach((fruit) => {
+  console.log(fruit);
+});
+
+// reverse();
+console.log(fruits9.reverse());
+
+// some()
+// returns boolean
+const arrayy = [9, 8, 1, 2, 3, 4, 5];
+
+const even = (num) => num % 2 === 0;
+const isDivByTwo = arrayy.some(even);
+// const isDivByTwo = arrayy.some((num) => num % 2 === 0);
+console.log(isDivByTwo);
+
+// sort()
+// depends on data type and is more complex
+console.log(fruits9.sort());
+console.log(arrayy.sort((a, b) => a - b));
+
+// map()
+const array1 = [1, 4, 9, 16];
+const array1Double = array1.map((num) => num * 2);
+console.log(array1Double);
+
+// filter()
+const words2 = [
+  'spray',
+  'limit',
+  'elite',
+  'exuberant',
+  'destruction',
+  'present',
+  'badword',
+];
+
+const biggerWords = words2.filter((word) => word.length > 6);
+console.log(biggerWords);
+
+const goodWords = words2.filter((word) => word !== 'badword');
+console.log(goodWords);
+
+// reduce()
+const prices = [1.95, 3, 50.5, 19];
+
+// froEach
+let sum = 0;
+prices.forEach((price) => {
+  sum += price;
+});
+console.log(sum);
+
+// reduce
+const totalSum = prices.reduce((sum, price) => {
+  return (sum += price);
+  //
+}, 0);
+console.log(totalSum);
+
+// Example2:
+let names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice', 'Bob'];
+let countedNames = names.reduce(function (allNames, name) {
+  if (name in allNames) {
+    allNames[name]++;
+  } else {
+    allNames[name] = 1;
+  }
+  return allNames;
+}, {});
+console.log(countedNames); //  { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
+
+// Example3
+var trash = [1, 'bag', 'bottle', 'paper', true];
+
+trash.pop();
+trash.push(true);
+
+console.log(trash);
+
+var filteredTrash = trash.filter(function (elem) {
+  if (elem == 'bag' || elem == 'bottle') {
+    return elem;
+  }
+});
+console.log('filteredTrash:', filteredTrash);
+var totallyTrash = trash.concat(filteredTrash);
+console.log('totallyTrash:', totallyTrash);
+totallyTrash.map(function (elem) {
+  // console.log(elem);
+});
