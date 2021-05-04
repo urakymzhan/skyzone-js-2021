12a0933d3b6d5e8fae85f2f2d5cefc515b3d5472
Ulan Rakymzhanov <ulan@carta.healthcare>
2021-05-04 18:02:01 -0500

    dom

--- 07_js-DOM/traverse.js
+++ 07_js-DOM/traverse.js
@@ -21,3 +21,49 @@ const children = document.children;
 // children.forEach((node) => {
 //   console.log('node', node);
 // }); // error
+
+// FROM NOW ON ---------------------------------------------------------------------------------
+// just an example from: https://www.digitalocean.com/community/tutorials/how-to-traverse-the-dom
+
+const h1 = document.getElementsByTagName('h1')[0];
+const p = document.getElementsByTagName('p')[0];
+const ul = document.getElementsByTagName('ul')[0];
+
+/**
+ * parentNode	=> Parent Node
+ * parentElement	=> Parent Element Node
+ */
+
+console.log(p.parentNode);
+console.log(p.parentNode.parentNode);
+
+// Assign html object to html variable
+const html = document.documentElement;
+console.log(html.parentNode); // > #document
+console.log(html.parentElement); // > null
+
+/**
+ * childNodes	=> Child Nodes
+ * firstChild	=> First Child Node
+ * lastChild	=> Last Child Node
+ * children	=> Element Child Nodes
+ * firstElementChild	=> First Child Element Node
+ * lastElementChild	=> Last Child Element Node
+ */
+
+console.log(ul.childNodes);
+// ul.firstElementChild.style.background = 'yellow';
+for (let element of ul.children) {
+  element.style.background = 'yellow';
+}
+
+/**
+ * previousSibling	=> Previous Sibling Node
+ * nextSibling	=> Next Sibling Node
+ * previousElementSibling	=> Previous Sibling Element Node
+ * nextElementSibling	=> Next Sibling Element Node
+ */
+
+const tiger = ul.children[1];
+tiger.nextElementSibling.style.background = 'coral';
+tiger.previousElementSibling.style.background = 'aquamarine';
