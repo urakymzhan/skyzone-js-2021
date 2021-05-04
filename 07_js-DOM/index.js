5b0cc92f5a2e55d7ac4002ab25b05e84e5c81439
Ulan Rakymzhanov <ulan@carta.healthcare>
2021-05-03 22:26:34 -0500

    dom

--- 07_js-DOM/index.js
+++ 07_js-DOM/index.js
@@ -96,3 +96,94 @@ appleEl.setAttribute('alt', '4 apples');
 
 console.log(`source`, appleEl.getAttribute('src'));
 console.log(`alt`, appleEl.getAttribute('alt'));
+
+// Styling of DOM elements
+
+const theBody = document.getElementsByTagName('body')[0];
+theBody.style.backgroundColor = 'lightgray';
+theBody.style.fontSize = '80%';
+
+appleEl.style.width = '200px';
+appleEl.style.height = 'auto';
+
+appleEl.style.border = '2px solid gray';
+appleEl.style.marginTop = '20px';
+appleEl.style.marginLeft = '20px';
+
+// className vs classList
+console.log(appleEl.className);
+console.log(appleEl.classList);
+
+appleEl.className = 'newAppleClassName';
+
+console.log(appleEl.className);
+console.log(appleEl.classList);
+
+appleEl.classList.add('red');
+
+console.log(appleEl.className);
+console.log(appleEl.classList);
+console.log(appleEl.classList[0]);
+console.log(appleEl.classList[1]);
+
+appleEl.classList.remove('newAppleClassName');
+
+console.log(appleEl.className);
+console.log(appleEl.classList);
+
+console.log(appleEl.classList.contains('red'));
+console.log(appleEl.classList.contains('green'));
+
+appleEl.classList.replace('red', 'green');
+
+console.log(appleEl.className);
+console.log(appleEl.classList);
+
+// toggle ex: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onclick_dropdown
+appleEl.classList.toggle('green'); // removed
+
+console.log(appleEl.className);
+console.log(appleEl.classList);
+
+appleEl.classList.toggle('green'); // brought back
+
+console.log(appleEl.className);
+console.log(appleEl.classList);
+
+// innerHTML
+const greetingBox = document.querySelector('.greeting');
+greetingBox.innerHTML = '<h1>Hello World</h1>'; // simlple
+greetingBox.innerHTML += '<p>This is hello world</p>'; // do not override
+
+// textContent
+const greetingBox2 = document.querySelector('.greeting2');
+greetingBox2.textContent = 'Hello World';
+
+// innerText
+greetingBox2.innerText = 'Hello World 2';
+
+/**
+ * Creating a New Node
+ */
+
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');
+// const text = document.createTextNode('Do not copy!');
+// copyright.appendChild(text);
+
+// appendChild()
+copyright.textContent = 'Do not copy!!!';
+footer.appendChild(copyright);
+
+// append()
+const address = document.createElement('p');
+address.textContent = 'Mars';
+address.append(' Pluton'); // DOMString ( simply text )
+footer.append(address);
+
+// Activity
+const theBody2 = document.getElementsByTagName('body')[0];
+const div123 = document.createElement('div');
+const newH1 = document.createElement('h1');
+newH1.textContent = 'hey this is H1';
+theBody2.appendChild(div123.appendChild(newH1));
